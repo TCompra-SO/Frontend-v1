c957d0389338029afb6dd10081752e81f3a1bd16
al.h8500@gmail.com <al.h8500@gmail.com>
2024-12-05 12:36:49 -0500

    Abrir documento e imagenes en mensaje de chat

--- src/components/section/chat/ChatBody/ChatBodyMessage.tsx
+++ src/components/section/chat/ChatBody/ChatBodyMessage.tsx
@@ -2,6 +2,11 @@ import dayjs from "dayjs";
 import { hourFormatChatBody } from "../../../../utilities/globals";
 import { openDocument } from "../../../../utilities/globalFunctions";
 import { ChatMessage } from "../../../../models/MainInterfaces";
+import {
+  ImagePreviewGroupContainer,
+  ImagePreviewGroupContainerRef,
+} from "../../../containers/ImagePreviewGroupContainer";
+import { useRef } from "react";
 
 interface ChatBodyMessageProps {
   message: ChatMessage;
@@ -10,72 +15,99 @@ interface ChatBodyMessageProps {
 }
 
 export default function ChatBodyMessage(props: ChatBodyMessageProps) {
+  const childRef = useRef<ImagePreviewGroupContainerRef>(null);
+
+  function handleOpenPreview() {
+    if (props.message.images && props.message.images.length > 0) {
+      if (childRef.current) {
+        childRef.current.openPreview();
+      }
+    }
+  }
+
   return (
-    <div
-      className={`t-flex ${
-        props.message.isInputMsg ? "txt-entrada" : "txt-salida"
-      }`}
-    >
-      {props.message.isInputMsg ? (
-        props.userImage ? (
-          <img src={props.userImage} className="useri-chat" />
-        ) : (
-          <div className="inicial-chat-2">
-            {props.userName && props.userName.length > 0
-              ? props.userName[0]
-              : null}
-          </div>
-        )
-      ) : (
-        !(props.message.images || props.message.documents) && (
-          <div className="space-img"></div>
-        )
-      )}
+    <>
+      <ImagePreviewGroupContainer ref={childRef} image={props.message.images} />
       <div
-        className={
-          props.message.images
-            ? `t-flex gap-5 ${
-                props.message.isInputMsg
-                  ? "mensaje-entrada-img"
-                  : "mensaje-salida-img"
-              }`
-            : props.message.documents
-            ? props.message.isInputMsg
-              ? "mensaje-entrada-doc"
-              : "mensaje-salida-doc"
-            : props.message.isInputMsg
-            ? "mensaje-entrada"
-            : "mensaje-salida"
-        }
+        className={`t-flex ${
+          props.message.isInputMsg ? "txt-entrada" : "txt-salida"
+        }`}
       >
-        {props.message.images ? (
-          props.message.images.map((img) => (
-            <img src={img} alt="" className="image-send" key={img} />
-          ))
-        ) : props.message.documents ? (
-          props.message.documents.map((doc) => (
-            <div className="file-min-2" onClick={() => openDocument(doc)}>
-              <i className="fa-regular fa-file-doc"></i>
+        {props.message.isInputMsg ? (
+          props.userImage ? (
+            <img src={props.userImage} className="useri-chat" />
+          ) : (
+            <div className="inicial-chat-2">
+              {props.userName && props.userName.length > 0
+                ? props.userName[0]
+                : null}
             </div>
-          ))
+          )
         ) : (
-          <div>{props.message.message}</div>
+          !(props.message.images || props.message.documents) && (
+            <div className="space-img"></div>
+          )
         )}
-        <span
-          className={props.message.images ? "mensaje-hora-img" : "mensaje-hora"}
+        <div
+          className={
+            props.message.images
+              ? `t-flex gap-5 ${
+                  props.message.isInputMsg
+                    ? "mensaje-entrada-img"
+                    : "mensaje-salida-img"
+                }`
+              : props.message.documents
+              ? props.message.isInputMsg
+                ? "mensaje-entrada-doc"
+                : "mensaje-salida-doc"
+              : props.message.isInputMsg
+              ? "mensaje-entrada"
+              : "mensaje-salida"
+          }
         >
-          {dayjs(props.message.time).format(hourFormatChatBody)}{" "}
-          <i
-            className={`fa-solid ${
-              props.message.read ? "fa-check-double" : "fa-check"
-            }`}
-          ></i>
-        </span>
+          {props.message.images ? (
+            props.message.images.map((img) => (
+              <img
+                src={img}
+                alt=""
+                className="image-send"
+                key={img}
+                onClick={handleOpenPreview}
+                style={{ cursor: "pointer" }}
+              />
+            ))
+          ) : props.message.documents ? (
+            props.message.documents.map((doc) => (
+              <div
+                className="file-min-2"
+                onClick={() => openDocument(doc)}
+                style={{ cursor: "pointer" }}
+                key={doc}
+              >
+                <i className="fa-regular fa-file-doc"></i>
+              </div>
+            ))
+          ) : (
+            <div>{props.message.message}</div>
+          )}
+          <span
+            className={
+              props.message.images ? "mensaje-hora-img" : "mensaje-hora"
+            }
+          >
+            {dayjs(props.message.time).format(hourFormatChatBody)}{" "}
+            <i
+              className={`fa-solid ${
+                props.message.read ? "fa-check-double" : "fa-check"
+              }`}
+            ></i>
+          </span>
+        </div>
+        {props.message.isInputMsg &&
+          !(props.message.images || props.message.documents) && (
+            <div className="space-img"></div>
+          )}
       </div>
-      {props.message.isInputMsg &&
-        !(props.message.images || props.message.documents) && (
-          <div className="space-img"></div>
-        )}
-    </div>
+    </>
   );
 }
