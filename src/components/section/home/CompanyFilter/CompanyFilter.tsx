f9fdf0e75cd3afe7459d93ae7c0ee37349e67963
Alexandra1123 <al.h8500@gmail.com>
2025-03-05 22:25:45 +0400

    Reset de empresa buscada en home al salir de página

--- src/components/section/home/CompanyFilter/CompanyFilter.tsx
+++ src/components/section/home/CompanyFilter/CompanyFilter.tsx
@@ -1,11 +1,16 @@
 import { useTranslation } from "react-i18next";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { HomeContext } from "../../../../contexts/Homecontext";
 import SelectCompanyField from "./SelectCompanyField";
 
 export default function CompanyFilter() {
   const { t } = useTranslation();
   const { updateUserId } = useContext(HomeContext);
+
+  useEffect(() => {
+    return () => updateUserId("");
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <div className="card-white back-filter">
